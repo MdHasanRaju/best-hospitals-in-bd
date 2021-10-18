ff9188127f5a231e514b621060fd3fb64f3c58b2
MdHasanRaju <mdhasanraju2017@gmail.com>
2021-10-19 00:31:29 +0600

    dynamic route added

--- src/components/HospitalDetails/HospitalDetails.js
+++ src/components/HospitalDetails/HospitalDetails.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Card, Container } from 'react-bootstrap';
 import { useParams } from 'react-router';
 
 const HospitalDetails = () => {
@@ -17,11 +18,18 @@ const HospitalDetails = () => {
     } ,[])
 
     return (
-        <div>
-            {
-                <h2>{service.name}</h2>
-            }
-        </div>
+      <Container>
+        <Card className="h-100 text-dark">
+          <Card.Img height="100%" className="w-100 " src={service.img} alt="Card image" />
+          <Card.ImgOverlay>
+            <Card.Title>{service.name}</Card.Title>
+            <Card.Text>
+              {service.desc}
+            </Card.Text>
+            <Card.Text>Last updated 3 mins ago</Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+      </Container>
     );
 };
 
